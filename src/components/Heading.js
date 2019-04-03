61c4bbb9f6e034e4c641d8bd173f8691bb8699bd
Austin Beaufort <tinyhatcoding@gmail.com>
2019-04-02 22:43:20 -0400

    initial commit

---
+++ src/components/Heading.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Heading = () => (
+    <div>
+         <h1>Counting Forever Challenge</h1>
+        <h3>How Many Times Can You Click The Button?</h3>   
+    </div>
+);
+
+export default Heading; 
